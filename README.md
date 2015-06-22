0cacbad3c94d10c2260880f4a2ff4594b6348132
Emily Kapor <emily@emilyaviva.com>
2015-06-22 12:51:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Anxiety Tracker
+
+A web app for short- and long-term tracking of anxiety.
+
+There's not much here yet.
---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('lodash');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/app/'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var adjective = new Adjective();
+var noun = new Noun();
+var verb = new Verb();
+
+app.post('/day', function(req, res) {
+  console.log('Nothing here yet');
+});
+
+app.get('/day', function(req, res) {
+  console.log('Nothing here yet');
+});
+
+app.listen(port, function() {
+  console.log('server listening on port', port);
+});
